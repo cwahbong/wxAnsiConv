c120d0e0d930a109736c31de8cd86458bb3e5b1d
cwahbong <cw.ahbong@gmail.com>
2014-09-08 17:43:23 +0800

    Rename source.

--- src/AnsiConvApp.cpp
+++ src/AnsiConvApp.cpp
@@ -4,45 +4,45 @@
 class AnsiConvApp: public wxApp
 {
 public:
-  virtual bool OnInit();
   virtual void OnInitCmdLine(wxCmdLineParser &);
   virtual bool OnCmdLineParsed(wxCmdLineParser &);
+  virtual int OnRun();
 
 private:
   wxString input_name;
   wxString output_name;
 };
 
-bool
-AnsiConvApp::OnInit()
-{
-  wxLog::SetActiveTarget(new wxLogStderr);
-  return wxApp::OnInit();
-}
-
 void
 AnsiConvApp::OnInitCmdLine(wxCmdLineParser &parser)
 {
   parser.AddOption(wxT_2("i"),
                    wxT_2("input"),
-                   wxT_2("The input ansi art file."));
+                   wxT_2("The input ansi art file."),
+                   wxCMD_LINE_VAL_STRING,
+                   wxCMD_LINE_OPTION_MANDATORY);
   parser.AddOption(wxT_2("o"),
                    wxT_2("output"),
-                   wxT_2("The output picture."));
+                   wxT_2("The output picture."),
+                   wxCMD_LINE_VAL_STRING,
+                   wxCMD_LINE_OPTION_MANDATORY);
 }
 
 bool
 AnsiConvApp::OnCmdLineParsed(wxCmdLineParser &parser)
 {
-  if (!parser.Found(wxT_2("i"), &input_name)) {
-    wxLogError(wxT_2("Input not specified."));
-    return false;
-  }
-  if (!parser.Found(wxT_2("o"), &output_name)) {
-    wxLogError(wxT_2("Output not specified."));
-    return false;
-  }
-  return true;
+  parser.Found(wxT_2("i"), &input_name);
+  parser.Found(wxT_2("o"), &output_name);
+  return wxApp::OnCmdLineParsed(parser);
+}
+
+int
+AnsiConvApp::OnRun()
+{
+  // TODO read input
+  // TODO generate wxImage
+  // TODO write image
+  return 0;
 }
 
 // This implements the main function.
